b654018110f2016cdc39cf93825e320d558c8fa2
Unknown <tetik99@gmx.de>
2021-11-12 20:59:59 +0100

    hidden added

--- L03_SequenzmemorySettings/seqmemory.js
+++ L03_SequenzmemorySettings/seqmemory.js
@@ -1,34 +1,29 @@
 var L03_Sequenz;
 (function (L03_Sequenz) {
-    var card;
-    var prepTime;
-    var gameTime;
-    var word;
-    var sequence;
-    var timer;
-    var gameField;
-    var formData;
+    var form;
+    var memoryField;
+    var startButton;
+    var sequence = [];
+    // let formData: FormData;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        var startButton = document.querySelector("#startButton");
-        startButton.addEventListener("pointerup", createSequenz);
-    }
-    function createSequenz(_event) {
-        formData = new FormData(document.forms[0]);
-        console.log(formData);
-        // prepTime = formData.get("prepTime");
-        // word = formData.get("word");
-        //gameTime = formData.get("gameTime");
-        var body = document.querySelector("body");
-        body.innerHTML = " ";
-    }
-    function createCard(_word) {
-        var card = document.createElement("div");
-        card.classList.add("card");
-        card.classList.add("hidden");
-        card.addEventListener("click", clickHandler);
+        form = document.querySelector("#settings");
+        form.addEventListener("change", clickHandler);
+        memoryField = document.querySelector("#field");
+        startButton = document.querySelector("#startButton");
+        startButton.addEventListener("click", createCard);
     }
     function clickHandler(_event) {
+        var formData = new FormData(document.forms[0]);
+        // sequence = [];
+        // for (let entry of formData) {
+        //     sequence.push(String(entry[1]));
+        //     console.log(entry);
+        // }    
+    }
+    function createCard(_word) {
+        form.classList.add("hidden");
+        startButton.classList.add("hidden");
     }
 })(L03_Sequenz || (L03_Sequenz = {}));
 //# sourceMappingURL=seqmemory.js.map
--- L03_SequenzmemorySettings/seqmemory.ts
+++ L03_SequenzmemorySettings/seqmemory.ts
@@ -1,46 +1,40 @@
 namespace L03_Sequenz {
     
-    let card: HTMLDivElement;
-    let prepTime: number;
-    let gameTime: number;
-    let word: string;
-    let sequence: string[];
-    let timer: HTMLSpanElement;
-    let gameField: HTMLDivElement;
-    let formData: FormData;
+    let form: HTMLElement;
+    let memoryField: HTMLElement;
+    let startButton: HTMLElement;
+    let sequence: string[] = [];
+    // let formData: FormData;
+
 
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
-        let startButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#startButton");
-        startButton.addEventListener("pointerup", createSequenz);
-    }
-
-    function createSequenz(_event: Event): void {
-
-        formData = new FormData(document.forms[0]);
-        console.log(formData);
-        
-        
-        // prepTime = formData.get("prepTime");
-        // word = formData.get("word");
-        //gameTime = formData.get("gameTime");
-
-        let body: HTMLBodyElement = <HTMLBodyElement>document.querySelector("body");
-        body.innerHTML = " ";
-
-    }
-
-    function createCard(_word: Event): void {
-        let card: HTMLDivElement = document.createElement("div");
-        card.classList.add("card");
-        card.classList.add("hidden");
-        card.addEventListener("click", clickHandler);
-
+        form = <HTMLElement>document.querySelector("#settings");
+        form.addEventListener("change", clickHandler);
+        memoryField = <HTMLDivElement>document.querySelector("#field");
+        startButton = <HTMLButtonElement>document.querySelector("#startButton");
+        startButton.addEventListener("click", createCard);
     }
 
     function clickHandler(_event: Event): void {
+
+        let formData: FormData = new FormData(document.forms[0]);
         
+        // sequence = [];
+
+        // for (let entry of formData) {
+        //     sequence.push(String(entry[1]));
+        //     console.log(entry);
+        // }    
     }
+
+    function createCard(_word: Event): void {
+        form.classList.add("hidden");
+        startButton.classList.add("hidden");
+
+
+    }
+
 
 }
