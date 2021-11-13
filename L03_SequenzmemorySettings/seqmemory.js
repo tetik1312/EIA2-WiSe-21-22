7aea69603674b27e75355b98989274f389bea5c1
Unknown <tetik99@gmx.de>
2021-11-13 19:13:04 +0100

    zwischenspeicher

--- L03_SequenzmemorySettings/seqmemory.js
+++ L03_SequenzmemorySettings/seqmemory.js
@@ -4,6 +4,8 @@ var L03_Sequenz;
     var memoryField;
     var startButton;
     var sequence = [];
+    var input;
+    var timer;
     // let formData: FormData;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
@@ -15,15 +17,44 @@ var L03_Sequenz;
     }
     function clickHandler(_event) {
         var formData = new FormData(document.forms[0]);
-        // sequence = [];
-        // for (let entry of formData) {
-        //     sequence.push(String(entry[1]));
-        //     console.log(entry);
-        // }    
     }
     function createCard(_word) {
-        form.classList.add("hidden");
-        startButton.classList.add("hidden");
+        input = prompt("enter your sequenz");
+        if (input == "" || input == null) {
+            alert("your funny bro, enter a sequenz");
+        }
+        else {
+            form.classList.add("hidden");
+            startButton.classList.add("hidden");
+            //let gamefield = document.createElement("div")
+            //gamefield.classList.add("card-content");
+            //let body = document.querySelector("body")
+            //let timer = document.createElement("span");
+            //timer.id = "timer";
+            //body.append(gamefield);
+            //body.append(timer);
+            sequence = input.split("");
+            var gamefield = document.createElement("div");
+            gamefield.classList.add("row");
+            for (var i = 0; i < input.length; i++) {
+                var random = Math.floor(Math.random() * sequence.length);
+                var card = document.createElement("div");
+                card.classList.add("column");
+                card.id = sequence.splice(random, 1).join();
+                var test = ("<div class='card'>" + card.id + "</div>");
+                card.innerHTML = test;
+                // newdiv.classList.add("");
+                //newdiv.style.background = document.getElementById('backgroundcolor').innerHTML;
+                var body = document.querySelector("body");
+                body.appendChild(gamefield);
+                gamefield.appendChild(card);
+                //document.body.style.backgroundColor = document.getElementById('backgroundcolor').innerHTML;
+            }
+            //createGame();
+        }
+    }
+    function createGame() {
+        timer = 5;
     }
 })(L03_Sequenz || (L03_Sequenz = {}));
 //# sourceMappingURL=seqmemory.js.map
--- L03_SequenzmemorySettings/seqmemory.ts
+++ L03_SequenzmemorySettings/seqmemory.ts
@@ -1,12 +1,12 @@
 namespace L03_Sequenz {
-    
+
     let form: HTMLElement;
     let memoryField: HTMLElement;
     let startButton: HTMLElement;
     let sequence: string[] = [];
+    let input: string;
+    let timer: number;
     // let formData: FormData;
-
-
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
@@ -16,25 +16,48 @@ namespace L03_Sequenz {
         startButton = <HTMLButtonElement>document.querySelector("#startButton");
         startButton.addEventListener("click", createCard);
     }
-
     function clickHandler(_event: Event): void {
 
         let formData: FormData = new FormData(document.forms[0]);
-        
-        // sequence = [];
-
-        // for (let entry of formData) {
-        //     sequence.push(String(entry[1]));
-        //     console.log(entry);
-        // }    
     }
 
     function createCard(_word: Event): void {
-        form.classList.add("hidden");
-        startButton.classList.add("hidden");
-
-
+        input = prompt("enter your sequenz");
+        if (input == "" || input == null) {
+            alert("your funny bro, enter a sequenz");
+        }
+        else {
+            form.classList.add("hidden");
+            startButton.classList.add("hidden");
+            //let gamefield = document.createElement("div")
+            //gamefield.classList.add("card-content");
+            //let body = document.querySelector("body")
+            //let timer = document.createElement("span");
+            //timer.id = "timer";
+            //body.append(gamefield);
+            //body.append(timer);
+            sequence = input.split("");
+            let gamefield: HTMLDivElement = document.createElement("div");
+            gamefield.classList.add("row");
+            for (let i: number = 0; i < input.length; i++) {
+                let random: number = Math.floor(Math.random() * sequence.length);
+                let card: HTMLDivElement = document.createElement("div");
+                card.classList.add("column");
+                card.id = sequence.splice(random, 1).join();
+                let test: string = ("<div class='card'>" + card.id + "</div>");
+                card.innerHTML = test;
+                // newdiv.classList.add("");
+                //newdiv.style.background = document.getElementById('backgroundcolor').innerHTML;
+                let body: HTMLBodyElement = document.querySelector("body");
+                body.appendChild(gamefield);
+                gamefield.appendChild(card);
+                //document.body.style.backgroundColor = document.getElementById('backgroundcolor').innerHTML;
+            }
+            //createGame();
+        }
     }
 
-
+    function createGame(): void {
+        timer = 5;
+    }
 }
